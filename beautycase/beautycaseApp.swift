bde2b86dbaf229848d08516ff9098f352b421a99
elysiumila666 <2411023640@qq.com>
2025-08-11 13:32:55 +0800

    Initial Commit

---
+++ beautycase/beautycaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  beautycaseApp.swift
+//  beautycase
+//
+//  Created by 何顺子 on 2025/8/11.
+//
+
+import SwiftUI
+
+@main
+struct beautycaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
